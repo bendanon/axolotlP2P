321bb89af6595c58c437d5caab668490c1da1e50
Guy Aloni <guy.aloni1@gmail.com>
2015-12-23 20:37:41 +0200

    Encrypt-decrypt UI

---
+++ java/src/main/java/ChatCommons/eUserStatus.java
@@ -0,0 +1,8 @@
+package ChatCommons;
+
+/**
+ * Created by Guy on 23/12/2015.
+ */
+public enum eUserStatus {
+    Wait, Trusted, Offline, UnTrusted
+}
---
+++ java/src/main/java/ChatGUI/UserRenderer.java
@@ -0,0 +1,42 @@
+package ChatGUI;
+
+import java.awt.Component;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+import ChatCommons.*;
+
+/**
+ * Created by Guy on 23/12/2015.
+ */
+public class UserRenderer extends JLabel implements ListCellRenderer<User>
+{
+    public UserRenderer()
+    {
+        setOpaque(true);
+    }
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends User> list, User user, int index,
+                                                  boolean isSelected, boolean cellHasFocus) {
+
+        eUserStatus status = user.GetUserStatus();
+        ImageIcon imageIcon = new ImageIcon(getClass().getResource("/images/" + status + ".png"));
+
+        setIcon(imageIcon);
+        setText(user.GetName());
+
+        if (isSelected)
+        {
+            setBackground(list.getSelectionBackground());
+            setForeground(list.getSelectionForeground());
+        } else
+        {
+            setBackground(list.getBackground());
+            setForeground(list.getForeground());
+        }
+
+        return this;
+    }
+}

fcec85b8b078e44e38a37a5a6b955de9eb013428
michael <mishkarapoport@gmail.com>
2015-12-30 18:05:02 +0200

    add message types enum

---
+++ java/src/main/java/ChatCommons/eMessageType.java
@@ -0,0 +1,12 @@
+package ChatCommons;
+
+/**
+ * Created by michael on 12/30/2015.
+ */
+public enum eMessageType {
+    eNORMAL,
+    eKEY_START,
+    eKEY_RESPONSE,
+    eKEY_FINISHED,
+    eWITNESS
+}
